74c8a7bbbdc76e4167a484e80f2ccd63b5ba2ebe
Zezinho11 <jcbrandine@gmail.com>
2026-06-04 15:08:28 -0300

    anti gravity test play

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import React1 from '../React1.jsx'
+import '../index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <React1 />
+  </React.StrictMode>,
+)
